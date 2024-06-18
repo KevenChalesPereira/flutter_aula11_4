75d1f9fa97d42e06a6fcf0110d8f7b7b909581ed
Kéven <69770399+KevenChalesPereira@users.noreply.github.com>
2024-06-17 22:01:56 -0300

    Resolução ex

--- lib/grade.dart
+++ lib/grade.dart
@@ -6,19 +6,21 @@ class Grade extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: Itens.letras.length,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
-        itemBuilder: (context, index) {
-          return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                color: Colors.blue,
-                child: Center(child: Text(Itens.letras[index])),
-              ));
-        });
+    return Scaffold(
+      body: GridView.builder(
+          itemCount: Itens.letras.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                  child: Center(child: Text(Itens.letras[index])),
+                ));
+          }),
+    );
   }
 }
